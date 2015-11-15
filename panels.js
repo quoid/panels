8783954a08156f1d8d3339581c22196deb903ffe
quoid <justinwasack@gmail.com>
2015-11-14 20:28:38 -0500

    v0.2.0 update

--- panels.js
+++ panels.js
@@ -1,213 +1,175 @@
-/*! panels by quoid v0.1.0 | GPL V2 License | http://quoid.github.io/panels/index.html */
-var a = 1; //1 = closed, 2 = open, 3 = opening
-var $trigger = $('[class*="-trigger"]');
-var $panelWrap = $('.panel-pusher');
-var timeoutLength = 250; //adjust to preference; prevents hyper clicking trigger (causes erratic behaviour)
-var transEnd = 'transitionend webkitTransitionEnd'; //css3 transition callbacks
+/*! panels by quoid v0.2.0 | GPL V2 License | http://quoid.github.io/panels/index.html */
+(function($) {
+    'use strict';
 
-function fixSafariScroll() { //fixes safari scroll bug
-    var $el = $('.panel-focus');
-    $el.css('overflow-y', 'hidden');
-    setTimeout(function() {
-        $el.css('overflow-y', 'auto');
-        $el.attr('style', '');
-    });
-}
+    var active = 1; //1 = closed, 2 = opening, 3 = open
+    var timeoutLength = 5000; 
+    var transEnd = 'transitionend webkitTransitionEnd'; //css3 transition callbacks
+    var $panelWrap = $('.panel-pusher');
 
-function preventTouchScroll() { //this is needed to prevent touch device (mobile) scrolling without the need for convoluted workarounds
-    $panelWrap.bind('touchmove', function(e) {
-        return false;
-    });
-}
+    $.panels = {
 
-function enableTouchScroll() {
-    $panelWrap.unbind('touchmove');
-}
+        show: function(options) {
+            var panelTarget = options['position'];
+            var a = '.panel-' + panelTarget;
+            var panelCount = $(a).length;
+            if (panelCount != 1) { //if the user added more than one panel of the same position, do nothing, print error
+	            console.error('only one panel element of the same position is allowed');
+	            return false;
+            } 
+            if(options["forceClose"] === true) {
+		    	$(a).addClass('panel-force-close');
+		    }
+		    if (options["timeOut"] !== null) {
+			    timeoutLength = options["timeOut"];
+		    }
+            switch (panelTarget) {
+                case 'top':
+                case 'bottom':
+                case 'left':
+                case 'right':
+                    if (active === 1) {
+                        showPanel(panelTarget);
+                        preventTouchScroll();
+                    } else if (active === 2) {
+                        console.error('panel animation in progress');
+                    } else {
+                        console.error('panel already shown');
+                    }
+                    break;
+                default:
+                    console.error('incorrect argument');
+                    break;
+            }
+	        
+        },
 
-$trigger.click(function(e) {
-    var triggerClass = $(this).attr('class').split(' ').filter(RegExp.prototype.test.bind(new RegExp('panel-'))).join().split('-');
-    var activeClass = triggerClass.slice(0, 2).join('-') + '-active';
-    var panelTarget = '.' + triggerClass[0] + '-' + triggerClass[1];
-    var panelHeight = $(panelTarget).outerHeight(true);
-    var panelWidth = $(panelTarget).outerWidth(true);
+        hide: function(options) {
+            var panelTarget = options['position'];
+            switch (panelTarget) {
+                case 'top':
+                case 'bottom':
+                case 'left':
+                case 'right':
+                    if (active === 1) {
+                        console.error('panel already hidden');
+                    } else if (active === 2) {
+                        console.error('panel already in progress');
+                    } else {
+	                   if (options["timeOut"] !== null) {
+						    timeoutLength = options["timeOut"];
+					    }
+                        hidePanel(panelTarget);
+                        enableTouchScroll();
+                    }
+                    break;
+                default:
+                    console.error('incorrect argument');
+                    break;
+            }
+        }
+    };
 
-    if (a == 1) {
-        $('body').addClass(activeClass);
-        $(panelTarget).addClass('panel-opening'); //immediately add panel opening class
-        preventTouchScroll(); //disable touch scroll
+    function showPanel(p1) {
+        var panel = '.panel-' + p1;
+        var panelHeight = $(panel).outerHeight(true);
+        var panelWidth = $(panel).outerWidth(true);
+        $('body').addClass('panel-' + p1 + '-active').css('overflow', 'hidden');
+        $(panel).addClass('panel-opening');
+        active = 2;
 
-        if ($(panelTarget).is('.push-panel, .reveal-panel')) {
-
-            switch (triggerClass[1]) {
-
+        if ($(panel).is('.push-panel, .reveal-panel')) {
+            switch (p1) {
                 case 'top':
                     $panelWrap.css('transform', "translate3d(0px, " + panelHeight + "px, 0px)");
                     break;
-
                 case 'bottom':
                     $panelWrap.css('transform', "translate3d(0px, " + -panelHeight + "px, 0px)");
                     break;
-
                 case 'left':
                     $panelWrap.css('transform', "translate3d(" + panelWidth + "px, 0px, 0px)");
                     break;
-
                 case 'right':
                     $panelWrap.css('transform', "translate3d(" + -panelWidth + "px, 0px, 0px)");
                     break;
-
             }
-
         }
-
-        a = 3;
-    } else if (a == 2) {
-        $('body').removeClass(activeClass);
-        $panelWrap.css('transform', ''); //remove inline styling from panel wrap
-        $(panelTarget).addClass('panel-closing');
-        a = 3;
-    } else {
-        return false;
     }
 
-});
+    function hidePanel(p1) {
+        var panel = '.panel-' + p1;
+        var b = 'panel-top-active panel-bottom-active panel-left-active panel-right-active';
+        $('body').removeClass(b);
+        $panelWrap.css('transform', '');
+        $(panel).addClass('panel-closing').removeClass('panel-force-close');
+        active = 2;
+    }
 
-$('.panel').on(transEnd, function(e) {
-
-    if (
-        e.originalEvent.propertyName == 'z-index' //we are using z-index to track trans end
-    ) {
-
-        if ($(this).hasClass('panel-opening')) { //if panel is opening
-            $(this).addClass('panel-focus'); //immediately after transition add the focus class
-            fixSafariScroll(); //fixes safari scroll bug
-            setTimeout(function() {
-                $('.panel-focus').removeClass('panel-opening'); //wait to remove panel-opening so panels don't get hyper triggered
-                a = 2;
-            }, timeoutLength);
-        } else if ($(this).hasClass('panel-closing')) { //if panel is closing
-            $(this).removeClass('panel-focus'); //immediately after transition is over remove the focus class
-            enableTouchScroll(); //enable touch (mobile) scrolling after panel is gone
-            setTimeout(function() {
-                $('.panel-closing').removeClass('panel-closing'); //wait to remove panel-closing so panels don't get hyper triggered
-                a = 1;
-            }, timeoutLength);
+    $('.panel').on(transEnd, function(e) {
+        if (e.originalEvent.propertyName == 'z-index') { //we are using z-index to track trans end
+            if ($(this).hasClass('panel-opening')) { //if panel is opening
+                $(this).addClass('panel-focus'); //immediately after transition, add the focus class
+                fixSafariScroll(); //fixes safari scroll bug
+                setTimeout(function() { //wait to remove panel-opening so panels don't get hyper triggered
+                    $('.panel-focus').removeClass('panel-opening');
+                    active = 3;
+                }, timeoutLength);
+            } else if ($(this).hasClass('panel-closing')) { //if panel is closing
+                $(this).removeClass('panel-focus'); //immediately after transition is over remove the focus class
+                $('body').attr('style', '');
+                enableTouchScroll(); //enable touch (mobile) scrolling after panel is gone
+                setTimeout(function() {
+                    $('.panel-closing').removeClass('panel-closing'); //wait to remove panel-closing so panels don't get hyper triggered
+                    active = 1;
+                }, timeoutLength);
+            }
         }
-
+    });
+    
+    $('body').click(function(e) {
+	    if (!$(e.target).closest('.panel').length && //if the element clicked is NOT the panel or panel-trigger
+	        !$('.panel-focus').hasClass('panel-force-close') && //and the user hasn't selected force close
+	        active == 3 //and the panel is showing
+	    ) {
+	        hidePanel('focus');
+	    }
+	});
+	
+	$('body').keydown(function(e) { //block key input from moving window when panel open
+	    var $keyParent = $(e.target).parent();
+	    var keys = [32, 33, 34, 35, 36, 37, 38, 39, 40]; //spacebar, pageup, pagedown, end, home, left arrow, up arrow, right arrow, down arrow
+	    var key = e.which;
+	    if (!$keyParent.hasClass('panel')) {
+	        switch (active) {
+	            case 1:
+	                break;
+	            case 2:
+	            case 3:
+	                if ($.inArray(key, keys) !== -1) {
+	                    return false;
+	                }
+	                break;
+	        }
+	    }
+	});
+	
+	function preventTouchScroll() { //this is needed to prevent touch device (mobile) scrolling without the need for convoluted workarounds
+        $panelWrap.bind('touchmove', function() {
+            return false;
+        });
     }
 
-});
-
-$('body').click(function(e) {
-    var activePanelClass = 'panel-top-active panel-bottom-active panel-left-active panel-right-active';
-
-    if (!$(e.target).closest('.panel, [class*="-trigger"]').length && //if the element clicked is NOT the panel or panel-trigger
-        !$('panel-focus').hasClass('panel-force-close') && //the panel in focus doesn't have force close class
-        a == 2 //and the panel is showing
-    ) {
-        $(this).removeClass(activePanelClass); //remove the body class
-        $panelWrap.css('transform', ''); //remove inline styling from panel wrap
-        $('.panel-focus').addClass('panel-closing'); //set the closing class on panel
-        a = 3; //mark as closing
+    function enableTouchScroll() {
+        $panelWrap.unbind('touchmove');
     }
 
-});
-
-$('body').keydown(function(e) { //block key input from moving window when panel open
-    var $keyParent = $(e.target).parent();
-    var keys = [32, 33, 34, 35, 36, 37, 38, 39, 40];
-    var key = e.which;
-
-    if (!$keyParent.hasClass('panel')) {
-
-        switch (a) {
-
-            case 1:
-                break;
-
-            case 2:
-            case 3:
-                if ($.inArray(key, keys) !== -1) {
-                    return false;
-                }
-                break;
-
-        }
-
+    function fixSafariScroll() { //fixes safari scroll bug
+        var $el = $('.panel-focus');
+        $el.css('overflow-y', 'hidden');
+        setTimeout(function() {
+            $el.css('overflow-y', 'auto');
+            $el.attr('style', '');
+        });
     }
-
-});
-
-//include a way to trigger panel without panel-trigger
-
-//might need to use panel wrapper opacity as the animator follow
-//panels.show({position: 'left'});
-
-Panels = { //uppercase so people know it is a class
 	
-	show: function (options) {
-		var b = 1;
-		var oPos = options['position'];
-		var panelWidthff = $('.panel-' + oPos).outerWidth(true);
-		var panelHeightff = $('.panel-' + oPos).outerHeight(true);
-		
-		switch (options['position']) {
-			
-			case 'top':
-				$('body').addClass('panel-left-active');
-				$('.panel-top').addClass('panel-opening');
-				preventTouchScroll();
-				if ($('.panel-top').is('.push-panel, .reveal-panel')) {
-					$panelWrap.css('transform', "translate3d(0px, " + panelHeightff + "px, 0px)");
-				}
-				a = 3;
-				break;
-				
-			case 'bottom':
-				$('body').addClass('panel-left-active');
-				$('.panel-bottom').addClass('panel-opening');
-				preventTouchScroll();
-				if ($('.panel-bottom').is('.push-panel, .reveal-panel')) {
-					$panelWrap.css('transform', "translate3d(0px, " + -panelHeightff + "px, 0px)");
-				}
-				
-				a = 3;
-				break;
-			
-			case 'left':
-				$('body').addClass('panel-left-active');
-				$('.panel-left').addClass('panel-opening');
-				preventTouchScroll();
-				if ($('.panel-left').is('.push-panel, .reveal-panel')) {
-					$panelWrap.css('transform', "translate3d(" + panelWidthff + "px, 0px, 0px)");
-				}
-				a = 3;
-				break;
-				
-			case 'right':
-				$('body').addClass('panel-left-active');
-				$('.panel-right').addClass('panel-opening');
-				preventTouchScroll();
-				if ($('.panel-right').is('.push-panel, .reveal-panel')) {
-					$panelWrap.css('transform', "translate3d(" + -panelWidthff + "px, 0px, 0px)");
-				}
-				a = 3;
-				break;	
-			
-		}
-		
-	},
-	
-	hide: function() {
-		var aPC = 'panel-top-active panel-bottom-active panel-left-active panel-right-active';
-		$('body').removeClass(aPC);
-		$panelWrap.css('transform', '');
-        $('.panel-focus').addClass('panel-closing');
-        a = 3;
-	}
-	
-};
-
-$('.cb').click(function(){
-	panels.show({position: 'left'});
-});
+}(jQuery));
